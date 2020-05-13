1b1fa18b1a1a7e3856e39bc2d6b5e18b8ea3e0fe
xiaowu <1181649328@qq.com>
2020-05-13 11:57:51 +0800

    processgfw

---
+++ FileProcess/processGfwData.java
@@ -0,0 +1,70 @@
+package FileProcess;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class processGfwData {
+    private List<String> gfwList = new ArrayList<>();
+    private List<String> newGfwList = new ArrayList<>();
+    private byte[] buffer = new byte[1000];
+    private String newGfwData;
+
+    private Logger logger = Logger.getGlobal();
+
+    public processGfwData(List<String> gfwList) {
+        this.gfwList = gfwList;
+
+    }
+
+    private void createFile(List<String> StrList) {
+        try (OutputStream output = new FileOutputStream("./test.txt")) {
+            for (String str : StrList) {
+                str = str + "\n";
+                output.write(str.getBytes("UTF-8"));
+            }
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+        }
+    }
+
+    /** process file */
+    public void replace(String ip, int port) {
+        String ipReg = "([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}";
+        Pattern ipPattern = Pattern.compile(ipReg);
+        Matcher ipVerify = ipPattern.matcher(ip);
+
+        if (ipVerify.matches() && port < 65535) {
+            String reg = ("(server=/[a-z0-9].*)/(([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})#([0-9]{1,5})");
+            Pattern p = Pattern.compile(reg);
+            for (String gfwData : gfwList) {
+                logger.fine("匹配数据：" + gfwData);
+                Matcher m = p.matcher(gfwData);
+                if (m.matches()) {
+                    logger.fine("匹配成功：" + m.group(0));
+                    newGfwData = m.group(1) + "/" + ip + "#" + port;
+                    newGfwList.add(newGfwData);
+                    logger.fine(newGfwData);
+                }
+            }
+            createFile(newGfwList);
+
+        } else {
+            logger.severe("输入的Ip或者端口不合法");
+            return;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ FileProcess/readGfwList.java
@@ -0,0 +1,52 @@
+package FileProcess;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class readGfwList {
+    private final String filePath;
+    private List<String> gfwList = new ArrayList<>();
+    private final Logger logger = Logger.getGlobal();
+
+    /** save filepath */
+    public readGfwList(final String filePath) {
+        this.filePath = filePath;
+        logger.setLevel(Level.INFO);/** 设置log lever */
+    }
+
+    /** read file */
+    private void readFile() {
+        final File gfwFile = new File(this.filePath);
+        try {
+            if (gfwFile.exists() && gfwFile.isFile()) {
+                logger.info("read file:" + gfwFile.getAbsolutePath());
+                final Scanner gfwData = new Scanner(Paths.get(gfwFile.getAbsolutePath()), "UTF-8");
+                while (gfwData.hasNextLine()) {
+                    gfwList.add(gfwData.nextLine());
+                }
+                logger.info("file read completed");
+            } else {
+                logger.severe("Not find file " + gfwFile.getAbsolutePath());
+            }
+        } catch (final Exception e) {
+            logger.severe("read File error:" + e.getMessage());
+        }
+    }
+
+    public List<String> getFileData() {
+        readFile();
+        return this.gfwList;
+    }
+
+    public static void main(final String[] args) {
+        final readGfwList read = new readGfwList("dnsmasq_gfwlist_ipset.conf");
+        List<String> gfwList = read.getFileData();
+        processGfwData processGfw = new processGfwData(gfwList);
+        processGfw.replace("127.0.10.1", 5353);
+    }
+}
